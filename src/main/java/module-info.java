c252aa8385ed57e21c9f3933f9b721cc6e736501
flashcoder237 <tefbasil@gmail.com>
2025-05-08 13:47:17 +0100

    images upload

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,7 @@ module com.ipes {
     requires java.desktop;
 
     opens com.ipes to javafx.fxml;
+    opens com.ipes.controller to javafx.fxml;
     exports com.ipes;
+    exports com.ipes.controller;
 }
